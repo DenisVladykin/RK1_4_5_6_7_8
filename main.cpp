f55a7a160e34a68ab09db5df4de19a07e781a109
DenisVladykin <vdv03@yandex.ru>
2022-04-11 20:59:33 +0300

    task_5

--- main.cpp
+++ main.cpp
@@ -7,108 +7,45 @@
 #include <QTextStream>
 #include <QtGlobal>
 #include <stdio.h>
+#include <QList>
 
 using namespace std;
-
-QByteArray filedata;
-
-void ReadFromFile(const char* filename)
+int sred(int*mas, int m)
 {
-    QFile f(filename);
-    f.open(QIODevice::ReadOnly);
-
-    filedata = f.readAll();
-
-    f.close();
-}
-
-char binNumb[32];
-void WriteToFile(const char* fileout)
-{
-    QFile f(fileout);
-    f.open(QIODevice::WriteOnly);
-
-    for(int i = 0; i < 32; i++)
+    int rez = 0;
+    for(int i = 0; i < m; i++)
     {
-        QTextStream writeStream(&f);
-        writeStream << binNumb[i];
+        //rez = rez + mas[i]/m; - данная конструкция работает некорректно. Если mas[i] не делится нацело на m идёт округлуние.
+        rez = rez + mas[i];
     }
-    f.close();
-}
-
-
-QString perevod(int numBinary) //Функция перевода бинарного числа в 16-чный формат
-{
-    QMap<QString, char> per;
-    QString rez;
-    rez.clear();
-    QVector<QString> bin;
-    int temp = numBinary;
-    while(temp > 0)
-    {
-        bin.push_back(QString::number(temp%10));
-        temp = temp/10;
-    }
-    while(bin.length()%4 != 0)
-    {
-        bin.push_back("0");
-    }
-    per["0000"] = '0';
-    per["0001"] = '1';
-    per["0010"] = '2';
-    per["0011"] = '3';
-    per["0100"] = '4';
-    per["0101"] = '5';
-    per["0110"] = '6';
-    per["0111"] = '7';
-    per["1000"] = '8';
-    per["1001"] = '9';
-    per["1010"] = 'A';
-    per["1011"] = 'B';
-    per["1100"] = 'C';
-    per["1101"] = 'D';
-    per["1110"] = 'E';
-    per["1111"] = 'F';
-
-    for(int i = 0; i < bin.length()/4; i++)
-    { //Необходимо проходить по массиву bin с конца. Тогда переводит верно
-        QString temp1;
-        temp1.clear();
-        for(int t = 0; t < 4; t++)
-        {
-            temp1.append(bin.at(bin.length()-i*4 - t - 1));
-        }
-        rez.append(per.lowerBound(temp1).value());
-    }
+    rez = rez/m;
     return rez;
 }
-
-void task3(QString inp)
+QList<int> task5(int* mas, int m, int n)
 {
-    int i = 0;
-    i = inp.toInt();
-    QString itog = perevod(i);
-    cout << "Result: ";
-    QTextStream out(stdout);
-    out << itog;
+    QList<int> rez;
+    for(int i = 0; i < n; i++)
+    {
+        int promrez;
+        promrez = sred(&mas[m*i], m);
+        rez.append(promrez);
+    }
+    return(rez);
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QString s;
-    int n;
-    cout << "Vvedite kol-vo simvolov: " << endl;
-    cin >> n;
-    cin.ignore();
-    for(int i = 0; i < n; i++)
+    int A[4][3] = {
+        {12, 80, 21},
+        {21, 24, 30},
+        {13, 30, 94},
+        {6, 11, 72}
+      };
+    QList<int> T = task5(*A, 3, 5);
+    for(int i = 0; i < T.length(); i++)
     {
-        cout << "Vvedite simvol :" << endl;
-        unsigned long im;
-        cin >> hex >> im;
-        char c = im;
-        s.append(c);
+        cout << "Average " << i+1 << ": " << T.at(i) << endl;
     }
-    task3(s);
     return a.exec();
 }
