020f064f9c8f990015e3e4a37d437f1d322bbaf3
DenisVladykin <vdv03@yandex.ru>
2022-04-12 15:59:11 +0300

    task_6

--- main.cpp
+++ main.cpp
@@ -10,145 +10,86 @@
 #include <QList>
 #include <string.h>
 
+
 using namespace std;
-
-
-//Указанная функция возвращает:
-//"-1" : если первая строка меньше второй(либо первая строка является подстрокой второй, но ее длина меньше);
-//"1" : если первая строка больше второй(либо вторая строка является подстрокой первой, но ее длина меньше);
-//"0" : если строки одинаковые(полное совпадение).
-//"-999" : неизвестная ошибка. Сбой в работе программы.
-int Mystrcmp(char* first, const char* second)
+struct Data
 {
-    int len = strlen(first);
-    int len1 = strlen(second);
-    int len2 = strlen(first);
-    if(len1<len)
+    int num;
+    char sim;
+};
+
+struct Node
+{
+    Node *next;
+    Node *prev;
+    struct Data d;
+};
+
+class LinkedList
+{
+    private:
+        Data d;
+        Node* HEAD;
+        Node* TAIL;
+        Node* curNode; // Текущий эл-т
+        int len;
+    public:
+        void push_back(const Data d);
+        struct Data pop_back();
+        struct Data pop_front();
+        void push_front(const Data&data);
+        Data nextNode();
+        bool search(int ID);
+        bool Swap(int ID1, int ID2);
+        void resetCurNode();
+        int listLen();
+};
+
+Data LinkedList::pop_back()
+{
+    if(TAIL!=nullptr) //Иначе len = 0 и цикл for в main не начнет выполняться.
     {
-        len = len1;
+        Node* temp = TAIL->prev;
+        Data tempd = TAIL->d;
+        temp->next = nullptr;
+        delete TAIL;
+        len--;
+        return tempd;
     }
-    for(int i = 0; i < len; i++)
-    {
-        if(int(first[i]) < int(second[i]))
-        {
-            return -1;
-        }
-        if(int(first[i]) > int(second[i]))
-        {
-            return 1;
-        }
-    }
-    if(len2>len1)
-    {
-        return 1;
-    }
-    if(len2<len1)
+}
+
+int task_6()
+{
+    LinkedList MyList;
+    QFile f("LinkedListData.txt");
+    f.open(QIODevice::ReadWrite);
+    Data ThisData;
+    int z = MyList.listLen();
+    if(z==0)
     {
         return -1;
     }
-    if(len2==len1)
+    for(int i = 0; i < z;i++)
     {
-        return 0;
+        ThisData = MyList.pop_back();
+        QTextStream writeStream(&f);
+        writeStream << "1. " << ThisData.num << " " << ThisData.sim << "\n";
     }
-    return -999;
+    f.close();
+    return 0;
 }
-
-struct StudentInfo {
-    char* name;
-    char* surname;
-    int age;
-    char* numTicket;
-};
-
-struct SubjMark {
-    char* subj;
-    int id_subj;
-    QVector<int> marks_list;
-    float averMark;
-
-    SubjMark() {
-        subj = new char[4]; //создать предмет по умолчанию - def с id '-1'
-        subj[0] = 'd';
-        subj[1] = 'e';
-        subj[2] = 'f';
-        subj[3] = '\n';
-        id_subj = -1;
-    }
-    SubjMark(const char* _subj) {
-        subj = new char[strlen(_subj)];
-        strcpy(subj, _subj);
-        id_subj = -1;
-    }
-
-    SubjMark(const char* _subj, int _idSubj) {
-        subj = new char[strlen(_subj)];
-        strcpy(subj, _subj);
-        id_subj = _idSubj;
-    }
-
-    ~SubjMark() {
-        if (subj != nullptr) {
-            delete [] subj;
-        }
-    }
-
-};
-
-
-class Student {
-private :
-    StudentInfo* info;
-    QList<SubjMark*> listSubjMark;
-
-    int lookForSub(const char* subjName) {
-        for (int i = 0; i < listSubjMark.size(); i++) {
-            if (Mystrcmp(listSubjMark[i]->subj, subjName) == 0) {
-                return i;
-            }
-        }
-
-        listSubjMark.push_back(new SubjMark(subjName));
-        return listSubjMark.size() - 1;
-    }
-
-public :
-
-    void addSubject(const char* subjName, int idSubj = 0) {
-        listSubjMark.push_back(new SubjMark(subjName, idSubj));
-    }
-
-    int addMarkToSubj(const char* subjName /*int _idSubj*/, int mark) {
-        int numSubj = lookForSub(subjName);
-
-        listSubjMark[numSubj]->marks_list.push_back(mark);
-        double temp = 0;
-        for(int i = 0; i < listSubjMark.length(); i++)
-        {
-            temp = 0;
-            for(int t = 0; t < listSubjMark.at(i)->marks_list.length(); t++)
-                temp = temp + listSubjMark.at(i)->marks_list.at(t);
-            temp = temp/listSubjMark.at(i)->marks_list.length();
-            listSubjMark.at(i)->averMark = temp;
-        }
-
-        return 0;
-    }
-    void checkstud()
-    {
-        cout << "Name: " << info->name << endl;
-        cout << "Surname: " << info->surname << endl;
-        cout << "Age: " << info->age << endl;
-        cout << "Num.Tick: " << info->numTicket << endl;
-        for(int i = 0; i < listSubjMark.length(); i++)
-        {
-            cout << "Score " << i << " : " << listSubjMark.at(i)->averMark;
-        }
-    }
-
-};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    int m = task_6();
+    if(m == -1)
+    {
+        cout << "Список пуст." << endl;
+    }
+    else
+    {
+        cout << "Данные успешно записаны." << endl;
+    }
     return a.exec();
 }
